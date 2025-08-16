57fb5197a79883923a516d1302927f284e88b1f6
Abhishek Pathania <codelikepathania@gmail.com>
2025-08-16 19:32:14 +0530

    d

---
+++ C++/Basic/math+cmath/cmathematics.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+              // absolute
+  cout << abs(-4.7) << "\n";
+  cout << abs(4.7) << "\n"; 
+  cout << abs(3) << "\n";
+//   acos
+  cout << acos(0.64) << "\n";
+  cout << acos(-0.4) << "\n";
+  cout << acos(0) << "\n";
+  cout << acos(1) << "\n";
+  cout << acos(-1) << "\n";
+  cout << acos(2) << "\n";
+//   acosh
+  cout << acosh(7) << "\n";
+  cout << acosh(56) << "\n";
+  cout << acosh(2.45) << "\n";
+//   asin
+  cout << asin(0.64) << "\n";
+  cout << asin(-0.4) << "\n";
+  cout << asin(0) << "\n";
+  cout << asin(1) << "\n";
+  cout << asin(-1) << "\n";
+  cout << asin(2) << "\n";
+
+  return 0;
+}
